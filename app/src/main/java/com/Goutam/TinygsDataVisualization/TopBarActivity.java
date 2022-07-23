5a94353603f51d882cb54520db92d238cd7afc7b
Goutam Verma <66783850+GoutamVerma@users.noreply.github.com>
2022-07-23 18:17:54 +0530

    Remove: Title Bar (#11)
    * Remove: Title Bar
    
    * Remove: Unused Window dependencies

--- app/src/main/java/com/Goutam/TinygsDataVisualization/TopBarActivity.java
+++ app/src/main/java/com/Goutam/TinygsDataVisualization/TopBarActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 import com.Goutam.TinygsDataVisualization.Packets.PacketsActivity;
 import com.Goutam.TinygsDataVisualization.Satelite.SateliteActivity;
@@ -14,6 +15,8 @@ public class TopBarActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_top_bar);
     }
 
